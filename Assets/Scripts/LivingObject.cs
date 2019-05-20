c4e0e361754ec62373916e5fe0e0c548ea7c0f66
Enderlook <enderlook@gmail.com>
2019-05-20 00:52:31 -0300

    Added shield bar to player
    Added shield bar to player.
    Refactorization of LivingObject class.

--- Assets/Scripts/LivingObject.cs
+++ Assets/Scripts/LivingObject.cs
@@ -53,29 +53,132 @@ public class LivingObject : MonoBehaviour {
     [Tooltip("Health bar script.")]
     public HealthBar healthBar;
 
+    /// <summary>
+    /// Initializes the bar's values, setting the fill of the main bar and returning the current value.
+    /// If startingValue is -1, startingMaximumValue will be used instead to fill the bar.
+    /// The returned value is the value used to fill the bar, which can be startingValue or statingMaximumValue (if startingValue is -1).
+    /// </summary>
+    /// <param name="bar"></param>
+    /// <param name="startingMaximumValue"></param>
+    /// <param name="startingValue"></param>
+    /// <returns></returns>
+    protected float InitializeBar(HealthBar bar, float startingMaximumValue, float startingValue)
+    {
+        float value = startingValue == -1 ? startingMaximumValue : startingValue;
+        bar.ManualUpdate(startingMaximumValue, value);
+        return value;
+    }
+
     protected virtual void Start()
     {
-        float health = startingHealth == -1 ? startingMaxHealth : startingHealth;
-        healthBar.ManualStart(startingMaxHealth, health);
-        Health = health;
+        /*float health = startingHealth == -1 ? startingMaxHealth : startingHealth;
+        healthBar.ManualUpdate(startingMaxHealth, health);*/
+        Health = InitializeBar(healthBar, startingMaxHealth, startingHealth);
         MaxHealth = startingMaxHealth;
     }
 
+    /// <summary>
+    /// Values must be positive.
+    /// </summary>
+    /// <param name="amount"></param>
     public void TakeHealing(float amount)
     {
-        if (amount < 0)
+        Health = ChangeValue(amount, Health, MaxHealth, true, "health");
+        /*if (amount < 0)
         {
-            Debug.LogWarning($"Healing amount was negative. The creature lose health.");
+            Debug.LogError("Healing amount was negative. The creature lose health.");
+
         }
-        Health += amount;
+        if (Health + amount > MaxHealth)
+            Health = MaxHealth;
+        else
+            Health += amount;*/
+        //ChangeValue(amount);
     }
+
+    /// <summary>
+    /// Values must be positive.
+    /// </summary>
+    /// <param name="amount"></param>
     public virtual void TakeDamage(float amount)
     {
-        if (amount < 0)
+        Health = ChangeValue(amount, Health, MaxHealth, false, "health");
+        /*if (amount < 0)
         {
-            Debug.LogWarning($"Damage amount was negative. The creature recover health.");
+            Debug.LogWarning("Damage amount was negative. The creature recover health.");
         }
-        Health -= amount;
+        if (Health - amount < 0)
+            Health = 0;
+        else
+            Health -= amount;*/
+        //ChangeValue(-amount);
+    }
+
+    /*private void ChangeValue(float amount)
+    {
+        if (Health + amount < 0)
+            Health = 0;
+        else if (Health + amount > MaxHealth)
+            Health = MaxHealth;
+        else
+            Health = amount;
+    }*/
+
+    // Can I use this? https://stackoverflow.com/questions/1402803/passing-properties-by-reference-in-c-sharp
+    /// <summary>
+    /// Amount must (should) be positive.
+    /// If isAdding is true, amount will be added to variable. On false, amount will be reduced to variable.
+    /// By any mean, variable will be greater than maximum or lower than 0. If that happens, the result will be replaced by the corresponding boundary and rest will store the difference between the surpassed value and the boundary.
+    /// The values returned are the result of variable +/- amount (determined by isAdding) and the rest. If there is no rest it'll be 0. 
+    /// </summary>
+    /// <param name="amount"></param>
+    /// <param name="variable"></param>
+    /// <param name="maximum"></param>
+    /// <param name="isAdding"></param>
+    /// <param name="keyword"></param>
+    /// <returns></returns>
+    protected /*System.Tuple<float, float>*/ (float value, float remain) ChangeValueWithRemain(float amount, float variable, float maximum, bool isAdding, string keyword)
+    {
+        //if ((shouldBePossitive && amount < 0) || (!shouldBePossitive && amount > 0))
+        if (amount < 0)
+            Debug.LogWarning($"{(isAdding ? "healing" : "damage")} amount was negative. The creature {(isAdding ? "decreasing" : "increasing")} {keyword}.");
+
+        if (!isAdding)
+            amount = -amount;
+
+        float rest = 0;
+                
+        if (variable + amount < 0)
+        {
+            rest = -(variable + amount);
+            variable = 0;
+        }
+        else if (variable + amount > maximum)
+        {
+            rest = variable + amount - maximum;
+            variable = MaxHealth;
+        }
+        else
+            variable += amount;
+
+        return /* new System.Tuple<float, float>(variable, rest)*/ (value: variable, remain: rest);
+    }
+
+    /// <summary>
+    /// Amount must (should) be positive.
+    /// If isAdding is true, amount will be added to variable. On false, amount will be reduced to variable.
+    /// By any mean, variable will be greater than maximum or lower than 0. If that happens, the result will be replaced by the corresponding boundary.
+    /// The value returned is the result of variable +/- amount (determined by isAdding).
+    /// </summary>
+    /// <param name="amount"></param>
+    /// <param name="variable"></param>
+    /// <param name="maximum"></param>
+    /// <param name="isAdding"></param>
+    /// <param name="keyword"></param>
+    /// <returns></returns>
+    protected float ChangeValue(float amount, float variable, float maximum, bool isAdding, string keyword)
+    {
+        return ChangeValueWithRemain(amount, variable, maximum, isAdding, keyword).value; //.Item1;
     }
 
     protected virtual void Die()
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -10,6 +10,52 @@ public class Player : LivingObject
     /*public float turnSpeed;
     public float rotationOffset;*/
 
+    [Tooltip("Maximum shield.")]
+    public float startingMaxShield = 100;
+    private float _maxShield;
+    protected float MaxShield {
+        get {
+            return _maxShield;
+        }
+        set {
+            _maxShield = value;
+            shieldBar.UpdateValues(Shield, MaxShield);
+        }
+    }
+    [Tooltip("Starting shield. Set -1 to use Max shield value.")]
+    public float startingShield = -1;
+    private float _shield;
+    protected float Shield {
+        get {
+            return _shield;
+        }
+        set {
+            _shield = value;
+            shieldBar.UpdateValues(Shield, MaxShield);
+        }
+    }
+
+    [Tooltip("Shield recharge rate (points per second).")]
+    public float shieldRechargeRate = 10;
+    [Tooltip("Amount of time in seconds after receive damage in order to start recharging shield.")]
+    public float shieldRechargeDelay = 3f;
+    private bool isRechargingShield = false;
+    //private Coroutine shieldRechargeCoroutine;
+
+    [Header("Build")]
+    [Tooltip("Shield bar script.")]
+    public HealthBar shieldBar;
+
+    protected override void Start()
+    {        
+        /*float shield = startingShield == -1 ? startingMaxShield : startingShield;
+        shieldBar.ManualUpdate(startingMaxHealth, shield);*/
+        Shield = InitializeBar(shieldBar, startingMaxShield, startingShield); ;
+        MaxShield = startingMaxShield;
+
+        base.Start();
+    }
+
     private void Update()
     {
         Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
@@ -23,14 +69,45 @@ public class Player : LivingObject
 
         // Move
         transform.position = Vector3.MoveTowards(transform.position, mousePosition, moveSpeed * Time.deltaTime);
+
+        // Recharge shield
+        if (isRechargingShield && Shield < MaxShield)
+            Shield = ChangeValue(shieldRechargeRate * Time.deltaTime, Shield, MaxShield, true, "shield");
+        /*// To avoid over-shield
+        if (Shield + shieldRechargeRate * Time.deltaTime < MaxShield)
+            Shield += shieldRechargeRate * Time.deltaTime;
+        else
+            Shield = MaxShield;*/
+
+        Debug.Log(Shield);
     }
 
     public override void TakeDamage(float amount)
     {
+        StopCoroutine(InitializeRechargeShieldDelay());
+        /*shieldRechargeCoroutine = */StartCoroutine(InitializeRechargeShieldDelay());
+        (float value, float rest) = ChangeValueWithRemain(amount, Shield, MaxShield, false, "shield");
+        Shield = value;
+        if (rest != 0)
+            Health = ChangeValue(rest, Health, MaxHealth, false, "health");
+
+
+
+        /*if (Shield - amount < 0)
+        {
+            Shield = 0;
+            float remainingDamage = amount - Shield;
+            base.TakeDamage(remainingDamage);
+        } else
+        {
+            Shield -= amount;
+        }*/
+
+        /*
         // Damage reduction on low health
         if (Health - amount < MaxHealth * 0.35f)
             amount = (Health - amount) + (amount - Health) * (10 + Mathf.Exp(1) / Mathf.Exp(-amount / 10));
-        base.TakeDamage(amount);
+        base.TakeDamage(amount);*/
     }
 
     protected override void Die()
@@ -40,5 +117,10 @@ public class Player : LivingObject
         base.Die();
     }
 
-
+    private IEnumerator InitializeRechargeShieldDelay()
+    {
+        isRechargingShield = false;
+        yield return new WaitForSeconds(shieldRechargeDelay);
+        isRechargingShield = true;
+    }
 }
