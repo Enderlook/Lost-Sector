eee2d8350fa89f0501f8a627bfba5c064a36049a
Enderlook <enderlook@gmail.com>
2019-05-26 11:52:40 -0300

    Enemy spawner rework (#1)
    EnemyPrefab now changes on the fly the weight rarity to spawn based on current difficulty.
    Remove worldDimensions .
    The ISpawningSubrutine (commented out) interface could be used to give special spawnings instructions to enemies which can't be given on the Start() or Awake() methods.
    The impulse of EnemyBase is no longer gave by EnemySpawner, but EnemyBase.Start().

--- Assets/Scripts/Masters/EnemySpawner.cs
+++ Assets/Scripts/Masters/EnemySpawner.cs
@@ -1,14 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using UnityEngine;
 
 public class EnemySpawner : MonoBehaviour
 {
-    // TODO: COMPLETE REWORK!!!
-    public GameObject enemyPrefab;    
-    //public Enemies test;
     public TransformRange[] spawnPoints;
+
+    public Enemies enemies;
 
     [Tooltip("Difficulty.")]
     public float difficulty = 1;
@@ -25,14 +25,6 @@ public class EnemySpawner : MonoBehaviour
     [Range(1, 600)]
     public float difficultyIncreaseInterval = 5;
 
-    private Vector3 worldDimensions;
-
-    private void Awake()
-    {
-        // https://answers.unity.com/questions/1527535/spawning-enemies-randomly-within-the-screen.html
-        worldDimensions = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, -10));
-    }
-
     private void Start()
     {
         InvokeRepeating("DifficultyIncrease", difficultyIncreaseInterval, difficultyIncreaseInterval);
@@ -41,17 +33,21 @@ public class EnemySpawner : MonoBehaviour
     
     IEnumerator SpawnWave()
     {
+        // TODO: This should stop on player death....
+        // TODO: Custom modifications per enemy.
         while (true)
         {
-            for (int i = 0; i < 5 + (Mathf.Log(difficulty, 2) * 2); i++)
+            foreach (GameObject enemyPrefab in enemies.GetEnemies(difficulty))
             {
                 TransformRange spawnRange = spawnPoints[Random.Range(0, spawnPoints.Length - 1)];
-                Vector3 position = spawnRange.getVector(); //new Vector3(Random.Range(-worldDimensions.x, worldDimensions.x), worldDimensions.y);
-                Vector3 impulse = new Vector2(Random.Range(0, 15), Random.Range(50, 300));
+                Vector3 position = spawnRange.getVector();
 
                 GameObject enemy = Instantiate(enemyPrefab, Global.enemiesParent);
                 enemy.transform.position = position;
-                enemy.GetComponent<EnemyBase>().impulse = -impulse; // - for downwards impulse
+
+                //https://forum.unity.com/threads/getcomponents-possible-to-use-with-c-interfaces.60596/
+                /*ISpawningSubrutine spawningSubrutine = enemy.GetComponent<ISpawningSubrutine>();
+                spawningSubrutine.Spawn();*/
 
                 yield return new WaitForSeconds(0.1f);
             }
@@ -85,63 +81,47 @@ public class EnemySpawner : MonoBehaviour
 
 }
 
-/*[System.Serializable]
-public class SpawnSide {
-    public enum SPAWN_SIDES { TOP, BOTTOM, LEFT, RIGHT, CUSTOM };
-
-    [Tooltip("Spawn position mode. If there are more than one, randomly will be chosen one")]
-    public SPAWN_SIDES[] spawnSides = new SPAWN_SIDES[] { SPAWN_SIDES.TOP };
-
-    /* TODO:
-     * https://forum.unity.com/threads/draw-a-field-only-if-a-condition-is-met.448855/#post-3435603
-     * https://answers.unity.com/questions/54010/is-it-possible-to-dynamically-disable-or-validate.html
-     */
-    /*[Tooltip("Custom spawning random position (only use if spawnSides is CUSTOM). Made in pairs of random spawning lines.")]
-    public TransformRange[] customSpawnPositions;
-
-    // TODO
-    [Tooltip("If wait until the before thing is done, and then wait Seconds Before Start in order to do something. If false, it won't wait until the thing before is done.")]
-    public bool waitUntilFinishBefore;
-    [Tooltip("Amount of seconds waited before start doing something.")]
-    public float secondsBeforeStart;
-    [Tooltip("Amount of seconds waited between each done thing.")]
-    public float intervalSeconds;
-    
-    private Vector3 GetSpawnLocation(Vector3 worldDimensions, SPAWN_SIDES side)
-    {
-        switch (side)
-        { // Not use break statement due to return usage
-            case SPAWN_SIDES.TOP:
-                return new Vector3(Random.Range(-worldDimensions.x, worldDimensions.x), worldDimensions.y);
-            case SPAWN_SIDES.BOTTOM:
-                return new Vector3(Random.Range(-worldDimensions.x, worldDimensions.x), -worldDimensions.y);
-            case SPAWN_SIDES.LEFT:
-                return new Vector3(-worldDimensions.x, Random.Range(-worldDimensions.y, worldDimensions.y));
-            case SPAWN_SIDES.RIGHT:
-                return new Vector3(-worldDimensions.x, Random.Range(-worldDimensions.y, worldDimensions.y));
-            case SPAWN_SIDES.CUSTOM:
-                TransformRange spawnPositionRange = customSpawnPositions[Random.Range(0, customSpawnPositions.Length) - 1];
-                return new Vector3(Random.Range(spawnPositionRange.startVector.x, spawnPositionRange.endVector.x), Random.Range(spawnPositionRange.startVector.x, spawnPositionRange.endVector.y));
-            default:
-                // https://stackoverflow.com/questions/105372/how-do-i-enumerate-an-enum-in-c
-                Debug.LogError(new System.Exception($"The side {side} isn't none of the possible values of SPAWN_SIDES = {System.Enum.GetValues(typeof(SPAWN_SIDES)).Cast<SPAWN_SIDES>().Select(e => $"{e} = {System.Convert.ToInt32(e)}")})"));
-                // If I don't add this, IDE says that not all the possible paths return a value
-                throw new System.Exception($"The side {side} isn't none of the possible values of SPAWN_SIDES = {System.Enum.GetValues(typeof(SPAWN_SIDES)).Cast<SPAWN_SIDES>().Select(e => $"{e} = {System.Convert.ToInt32(e)}")})");
-        }
-    }
-
-    private Vector3 GetSpawnLocation(Vector3 worldDimensions)
-    {
-        return GetSpawnLocation(worldDimensions, spawnSides[Random.Range(0, spawnSides.Length) - 1]);
-    }
-}*/
-
-/*[System.Serializable]
+[System.Serializable]
 public class EnemyPrefab {
     [Tooltip("Enemy prefab to spawn.")]
     public GameObject prefab;
     [Tooltip("Weighted rarity.")]
-    public float weight;
+    public float weight = 1;
+    [Tooltip("Threat level of the enemy. Used to not spawn a few weak enemies nor a lot o strong enemies.")]
+    public float threat;
+
+    [Tooltip("Minimal difficulty in order to allow the spawn of this enemy.")]
+    public float minimalDifficulty;
+    [Tooltip("Multiplies the weight rarity based on difficulty above the Minimal Difficulty. The letter x will be replaced by the difference between difficulty and Minimal Difficulty.")]
+    public string weightFactorFormula = "1";
+
+    /// <summary>
+    /// Return the weigthed rarity of the enemy to spawn. The weight changes according to the current difficulty.
+    /// </summary>
+    /// <param name="difficulty">Difficulty used to calculate weight.</param>
+    /// <returns>Weighted rarity.</returns>
+    public float GetWeight(float difficulty)
+    {
+        if (difficulty < minimalDifficulty)
+            return 0;
+        else
+        {
+            // TODO: This could be done safer using a custom math class...
+            // https://stackoverflow.com/questions/333737/evaluating-string-342-yield-int-18
+            DataTable dataTable = new DataTable();
+            float multiplier = float.Parse(dataTable.Compute(weightFactorFormula.Replace("x", difficulty.ToString()), "").ToString());
+            return weight * multiplier;
+        }
+    }
+
+    private void OnValidate()
+    {
+        if (weight <= 0)
+            Debug.LogWarning($"The field {nameof(weight)} can't be lower or equal than 0.");
+        if (prefab == null)
+            Debug.LogWarning($"The field {nameof(weight)} can't be null.");
+        // How to check minimalThreat?
+    }
 }
 
 [System.Serializable] 
@@ -149,9 +129,14 @@ public class Enemies {
     [Tooltip("Enemy prefabs to spawn.")]
     public EnemyPrefab[] enemyPrefabs;
 
-    public GameObject GetEnemyPrefab()
+    /// <summary>
+    /// Get an enemy prefab to spawn.
+    /// </summary>
+    /// <param name="difficulty">Current difficulty used to base the type of enemy.</param>
+    /// <returns>Enemy prefab to spawn</returns>
+    /*public GameObject GetEnemyPrefab(float difficulty)
     {
-        float totalWeight = enemyPrefabs.Sum((enemy) => enemy.weight);
+        float totalWeight = enemyPrefabs.Sum((enemy) => enemy.GetWeight(difficulty));
         float chosenWeight = Random.value * totalWeight;
 
         float currentWeight = 0;
@@ -164,10 +149,48 @@ public class Enemies {
             }
         }
         throw new System.Exception("This shouldn't be happening!!!");
+    }*/
+
+    /// <summary>
+    /// Get an enemy prefab to spawn and its treat.
+    /// </summary>
+    /// <param name="difficulty">Current difficulty used to base the type of enemy.</param>
+    /// <returns>Enemy prefab to spawn and threat of the enemy</returns>
+    private System.Tuple<GameObject, float> GetEnemy(float difficulty)
+    {
+        float totalWeight = enemyPrefabs.Sum((enemy) => enemy.GetWeight(difficulty));
+        float chosenWeight = Random.value * totalWeight;
+
+        float currentWeight = 0;
+        foreach (EnemyPrefab enemy in enemyPrefabs)
+        {
+            currentWeight += enemy.weight;
+            if (currentWeight >= chosenWeight)
+            {
+                return new System.Tuple<GameObject, float>(enemy.prefab, enemy.threat);
+            }
+        }
+        throw new System.Exception("This shouldn't be happening!!!");
     }
-}*/
 
-
+    /// <summary>
+    /// Return a list of enemies ready to be spawned based on the game difficulty.
+    /// </summary>
+    /// <param name="difficulty">Current difficulty</param>
+    /// <returns>List of enemies prefab to spawn.</returns>
+    public List<GameObject> GetEnemies(float difficulty)
+    {
+        float threat = 0;
+        List<GameObject> enemies = new List<GameObject>();
+        while (threat < 5 + (Mathf.Log(difficulty, 2) * 2))
+        {
+            System.Tuple<GameObject, float> enemy = GetEnemy(difficulty);
+            threat += enemy.Item2;
+            enemies.Add(enemy.Item1);
+        }
+        return enemies;
+    }
+}
 
 [System.Serializable]
 public class TransformRange {
