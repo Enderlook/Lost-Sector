6cc344e1cb99188a96f419206be538b669dcecaf
Ender Look <enderlook@gmail.com>
2019-05-28 09:40:41 -0300

    Add Enemy Kamikaze

---
+++ Assets/Scripts/Enemies/EnemyKamikaze.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyKamikaze : EnemyBase
+{
+    [Header("Configuration")]
+    [Tooltip("Seconds waited before start accelerating.")]
+    public float waitSecondsBeferoAccelerate;
+    [Tooltip("Acceleration in units per second.")]
+    public float acceleration;
+
+    private bool isAccelerating = false;
+    private float secondsWaitedBeforeAccelerate = 0;
+
+    private void Update()
+    {
+        if (isAccelerating)
+            rigidbodyHelper.GetRigidbody2D().AddRelativeForce(new Vector2(0, acceleration));
+        else
+        {
+            secondsWaitedBeforeAccelerate += Time.deltaTime;
+            isAccelerating = secondsWaitedBeforeAccelerate > waitSecondsBeferoAccelerate;
+        }
+    }
+}
