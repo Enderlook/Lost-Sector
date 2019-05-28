695ae0c6da47363cbc9c3ca16669291eaeec397c
Ender Look <enderlook@gmail.com>
2019-05-28 11:03:27 -0300

    Add explosion radius to Enemy Asteroid Explosive
    Replace EnemyAsteroid class for the new EnemyAsteroidExplosive on Enemy Asteroid Explosive prefab.

---
+++ Assets/Scripts/Enemies/Asteroids/EnemyAsteroidExplosive.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAsteroidExplosive : EnemyAsteroid
+{
+    [Header("Configuration")]
+    [Tooltip("Explosion radius.")]
+    public float explosionRadius;
+    [Tooltip("Explosion damage.")]
+    public float explosionDamage;
+    // TODO: Add explosion damage falls-off over distance
+
+    private bool hasAlreadyExploded = false;
+
+    protected override void Die()
+    {
+        if (!hasAlreadyExploded)
+        {
+            hasAlreadyExploded = true;
+            // TODO: Add explosion graphics
+
+            // https://answers.unity.com/questions/532746/finding-gameobjects-within-a-radius.html
+            // https://forum.unity.com/threads/find-gameobjects-in-a-circular-range-of-a-point.36197/
+            // We can also do this without Physics2D.OverlapCircleAll...
+            Collider2D[] colliders = Physics2D.OverlapCircleAll(rigidbodyHelper.Position, explosionRadius);
+            foreach (Collider2D collider in colliders)
+            {
+                // Avoid Recursivity (TakeDamage -> Die -> Explode -> TakeDamage -> ...)
+                Rigidbody2D rigidbody2D = collider.attachedRigidbody;
+                if (rigidbody2D != rigidbodyHelper.GetRigidbody2D())
+                {
+                    // This won't cause NullPointerException because the || clause will only be revised if the explose == null is false, which means there is an explosive to point.
+                    rigidbody2D.GetComponent<RigidbodyHelper>().TakeDamage(explosionDamage);
+                }
+            }
+        }
+        base.Die();
+    }
+    private void OnDrawGizmos()
+    {
+        UnityEditor.Handles.color = Color.red;
+        UnityEditor.Handles.DrawWireDisc(rigidbodyHelper.Position, Vector3.back, explosionRadius);
+    }
+}
