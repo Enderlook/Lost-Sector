2d26a692fc46d9457cc8184599aa5365a08a4a92
Ender Look <enderlook@gmail.com>
2019-06-12 23:03:59 -0300

    Add explosion animation
    Create SelfDestroyEndAnimation.cs
    Add explosion animation and SelfDestroyEndAnimation script  on Explosion prefab.
    Create Animations folder.

---
+++ Assets/Scripts/Add-Ons/SelfDestroyEndAnimation.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelfDestroyEndAnimation : MonoBehaviour
+{
+    private void Start()
+    {
+        Animator animator = gameObject.GetComponent<Animator>();
+        if (animator != null)
+            Destroy(gameObject, animator.runtimeAnimatorController.animationClips[0].length);
+    }
+
+    private void OnValidate()
+    {
+        Animator animator = gameObject.GetComponent<Animator>();
+        if (animator != null)
+            Debug.LogWarning($"Game object {gameObject.name} lacks of an Animator Component.");
+    }
+}
