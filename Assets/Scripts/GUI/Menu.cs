a14a9b5d9fb6cdcbb108f522cf0c73dc6b51751a
Ender Look <enderlook@gmail.com>
2019-06-13 16:26:02 -0300

    Rework Menu
    Rework in Main Menu and added a Menu on the level.
    Fix Sound mute check button and move menu in Level_1.unity

---
+++ Assets/Scripts/GUI/Menu.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+    [Header("Setup")]
+    [Tooltip("Menu to display on escape key press.")]
+    public GameObject menu;
+
+    private bool isActive;
+
+    private void Start() => DisplayMenuPause(false);
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+            DisplayMenuPause();
+    }
+
+    /// <summary>
+    /// Toggle visibility of the menu using the opposite value of <seealso cref="isActive"/>. <seealso cref="isActive"/> is set as its opposite value.<br/>
+    /// If <paramref name="active"/> isn't null this value will override the toggle.
+    /// </summary>
+    /// <param name="active">Whenever the menu is visible or not.</param>
+    public void DisplayMenuPause(bool? active = null)
+    {
+        isActive = active != null ? (bool)active : !isActive;
+        Time.timeScale = isActive ? 0 : 1;
+        menu.SetActive(isActive);
+    }
+
+    /// <summary>
+    /// Hide the menu and set to <see langword="false"/> <seealso cref="isActive"/>.
+    /// </summary>
+    public void HideMenu() => DisplayMenuPause(false);
+
+    /// <summary>
+    /// Reload the current scene.
+    /// </summary>
+    private void Reload() => SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+
+}
---
+++ Assets/Scripts/GUI/Settings.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class Settings : MonoBehaviour
+{
+    public static bool IsSoundActive = false;
+
+    /*private void Start()
+    {
+        // Make a persistent game object through scenes
+        DontDestroyOnLoad(gameObject);
+    }*/
+    public void Play() => SceneManager.LoadScene("Level_1", LoadSceneMode.Single);
+
+    public void SetSound(bool active) => IsSoundActive = active;
+
+    public void Exit() => Application.Quit();
+}
