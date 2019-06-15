c711aba65d3b0ed6bbbd833e0a458df036a806cf
Ender Look <enderlook@gmail.com>
2019-06-15 20:09:05 -0300

    Add coins and reorganize game objects
    Add Coin prefab.
    Add Coin class and CoinController class (to spawn coins).
    Add PickupMagnet class to collect coins and IPickup interface to pickup them.
    Add EnemyBase.Die() override to spawn coins. Also add public CoinController coinController and public FloatRangeTwo moneySpawnedOnDeath to spawn money.
    Player capture those coins.
    Add new classes to prefabs.
    Add coins sprites and animations.
    Reorganize gamobjects in Level_1.unity.

---
+++ Assets/Scripts/Add-Ons/CoinController.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using UnityEngine;
+
+public class CoinController : MonoBehaviour
+{
+    [Header("Configuration")]
+    [Tooltip("Random movement speed.")]
+    public FloatRangeTwo movementSpeed;
+
+    [Header("Setup")]
+    [Tooltip("Transform to spawn coins.")]
+    public Transform spawninigTransform;
+    [Tooltip("Coin prefab to spawn.")]
+    public GameObject coinPrefab;
+
+    // We want to always check in this order later...
+    private static readonly OrderedDictionary PRICES_TABLE = new OrderedDictionary()
+    {
+        { "gold", 100 }, { "silver", 10 }, { "copper", 1 }
+    };
+
+    /// <summary>
+    /// Spawn money.
+    /// </summary>
+    /// <param name="money">Amount of money to spawn.</param>
+    public void SpawnCoins(int money)
+    {
+        if (money > 0)
+            SpawnCoinsWithConfig(CalculateCoins(money));
+    }
+
+    /// <summary>
+    /// Spawn coins.
+    /// </summary>
+    /// <param name="coinsToSpawn">Amount of each type of coin</param>
+    private void SpawnCoinsWithConfig(IEnumerable<System.Tuple<string, int>> coinsToSpawn)
+    {
+        foreach (System.Tuple<string, int> coinToSpawn in coinsToSpawn)
+        {
+            for (int i = 0; i < coinToSpawn.Item2; i++)
+            {
+                GameObject coin = Instantiate(coinPrefab, Global.pickupsParent);
+
+                coin.transform.position = spawninigTransform.position;
+
+                Coin coinScript = coin.GetComponent<Coin>();
+
+                coinScript.SetConfiguration((int)PRICES_TABLE[coinToSpawn.Item1], coinToSpawn.Item1);
+
+                // https://answers.unity.com/questions/1111106/add-force-in-random-direction-with-set-speed.html
+                coin.GetComponent<Rigidbody2D>().velocity = Random.insideUnitCircle * movementSpeed.Value;
+            }
+        }
+    }
+
+    // We could use a Dictionary<string, int> also...
+    /// <summary>
+    /// Calculate the coins that should be spawned.
+    /// </summary>
+    /// <param name="price">Money worth of coins</param>
+    /// <returns><c>System.Tuple<string, int></c> of type of coin and its amount.</returns>
+    private IEnumerable<System.Tuple<string, int>> CalculateCoins(int price)
+    {
+        foreach (DictionaryEntry coin in PRICES_TABLE)
+        {
+            int amount = price / (int)coin.Value;
+            yield return new System.Tuple<string, int>(coin.Key.ToString(), amount);
+            price -= amount * (int)coin.Value;
+        }
+    }
+}
---
+++ Assets/Scripts/Others/Coin.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+
+public class Coin : MonoBehaviour, IPickup
+{    
+    private int price;
+    private Transform player;
+    private Rigidbody2D thisRigidbody2D;
+    private Animator animator;
+    private void Start() => thisRigidbody2D = gameObject.GetComponent<Rigidbody2D>();
+
+    /// <summary>
+    /// Destroy this coin and collect its money.
+    /// </summary>
+    void IPickup.Pickup()
+    {
+        Global.money += price;
+        Destroy(gameObject);
+    }
+
+    Rigidbody2D IPickup.Rigidbody2D {
+        get {
+            return thisRigidbody2D;
+        }
+    }
+
+
+    /// <summary>
+    /// Set configuration of the coin.
+    /// </summary>
+    /// <param name="price">Amount of money this coin is worth.</param>
+    /// <param name="animationState">Animation played by the coin.</param>
+    public void SetConfiguration(int price, string animationState)
+    {
+        this.price = price;
+        gameObject.GetComponent<Animator>().Play(animationState);
+    }
+
+    private void OnValidate()
+    {
+        if (gameObject.GetComponent<Animator>() == null)
+            Debug.LogWarning($"Game object {gameObject.name} lacks of an Animator Component.");
+        if (gameObject.GetComponent<Rigidbody2D>() == null)
+            Debug.LogWarning($"Game object {gameObject.name} lacks of an Rigidbody2D Component.");
+    }
+}
+
+// We may have more pickups in the future
+public interface IPickup
+{
+    /// <summary>
+    /// Method executed when the object must be picked up.
+    /// </summary>
+    void Pickup();
+    /// <summary>
+    /// <seealso cref="Rigidbody2D"/> of this pickup.
+    /// </summary>
+    Rigidbody2D Rigidbody2D { get; } 
+}
