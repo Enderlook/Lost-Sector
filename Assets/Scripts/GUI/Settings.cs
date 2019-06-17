d2ddcf1c991a4d4809649452710071961c728eb5
Ender Look <enderlook@gmail.com>
2019-06-17 00:30:51 -0300

    Improvement Menu
    Rename Assets\Scene\Menu.unity to Assets\Scene\Main_Menu.unity.
    Move code from Menu.Reload to Settings.Restart.
    The Exit button from Level_1 Menu no longer close the game, instead load Main_Menu.
    Add Restart button on Level_1 Menu.
    Documented and refactor code.

--- Assets/Scripts/GUI/Settings.cs
+++ Assets/Scripts/GUI/Settings.cs
@@ -11,9 +11,38 @@ public class Settings : MonoBehaviour
         // Make a persistent game object through scenes
         DontDestroyOnLoad(gameObject);
     }*/
-    public void Play() => SceneManager.LoadScene("Level_1", LoadSceneMode.Single);
 
+    /// <summary>
+    /// Load the Level_1 scene.
+    /// </summary>
+    public void Play() => LoadScene("Level_1");
+
+    /// <summary>
+    /// Set if the sound is played or muted.
+    /// </summary>
+    /// <param name="active">If <see langword="true"/> sound will be played. On <see langword="false"/> sound is muted.</param>
     public void SetSound(bool active) => IsSoundActive = active;
 
+    /// <summary>
+    /// Close game.
+    /// </summary>
     public void Exit() => Application.Quit();
+
+    /// <summary>
+    /// Reload the current scene.
+    /// </summary>
+    public void Restart() => LoadScene(SceneManager.GetActiveScene().name);
+
+    /// <summary>
+    /// Load the main menu scene.
+    /// </summary>
+    public void MainMenu() => LoadScene("Main_Menu");
+
+    /// <summary>
+    /// Load an scene.\n
+    /// Equivalent to <c><seealso cref="SceneManager.LoadScene"/>(<paramref name="scene"/>, <seealso cref="LoadSceneMode.Single"/>);</c>
+    /// </summary>
+    /// <seealso cref="SceneManager.LoadScene(string)"/>
+    /// <param name="scene">Scene name to load.</param>
+    private void LoadScene(string scene) => SceneManager.LoadScene(scene, LoadSceneMode.Single);
 }
