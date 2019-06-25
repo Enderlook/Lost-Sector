e0b947b2651163c8a55790cababcfec2a9f39083
Ender Look <enderlook@gmail.com>
2019-06-25 20:41:35 -0300

    Add rotation

--- Assets/Scripts/Enemies/Ships/EnemyShooter.cs
+++ Assets/Scripts/Enemies/Ships/EnemyShooter.cs
@@ -16,14 +16,44 @@ public class EnemyShooter : EnemyBase
     
     private Rigidbody2D thisRigidbody2D;
 
+    private float targetRotation;
     protected override void Start()
     {
         thisRigidbody2D = rigidbodyHelper.GetRigidbody2D();
+        targetRotation = thisRigidbody2D.rotation;
         base.Start();
     }
 
     private void Update()
     {
+        // Rotate
+        // TODO: Rotation should be fixed using angular velocity not rotation
+        float currentRotation = thisRigidbody2D.rotation;
+        float isClockwise = (((thisRigidbody2D.rotation - targetRotation + 360) % 360) > 180) ? 1 : -1;
+        float rotation = Time.deltaTime * rotationSpeed;
+        if (thisRigidbody2D.angularVelocity != 0)
+        {
+            thisRigidbody2D.angularVelocity *= 0.75f + Time.deltaTime * 0.25f;
+            if (Mathf.Abs(thisRigidbody2D.angularVelocity) < rotation || thisRigidbody2D.angularVelocity == 0)
+                thisRigidbody2D.angularVelocity = 0;
+            else
+                thisRigidbody2D.angularVelocity += rotation * (thisRigidbody2D.angularVelocity > 0 ? -1 : 1);
+        }
+        else
+        {
+            if (((thisRigidbody2D.rotation < targetRotation) && (thisRigidbody2D.rotation + rotation > targetRotation)) ||
+                ((thisRigidbody2D.rotation > targetRotation) && (thisRigidbody2D.rotation + rotation < targetRotation)) ||
+                thisRigidbody2D.rotation == targetRotation)
+                thisRigidbody2D.rotation = targetRotation;
+            else
+            {
+                thisRigidbody2D.rotation += rotation * isClockwise;
+            }
+        }
+
+        //Debug.Log(thisRigidbody2D.angularVelocity);
+        //Debug.Log(Mathf.Abs(thisRigidbody2D.angularVelocity) < rotation);
+
         // Slowly accelerate to crusierSpeed
         Vector2 speedToReachCrusier = cruiserSpeed - thisRigidbody2D.velocity;
         Vector2 forceToReachCrusier = speedToReachCrusier * thisRigidbody2D.mass;
