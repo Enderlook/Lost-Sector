d01bc5ea910af3a5f4baa734a4f961a8a3c5e793
Ender Look <enderlook@gmail.com>
2019-06-25 20:40:17 -0300

    Add Enemy Shooter
    Edit Enemy Kamikaze prefab.
    Update Projectile.SetprojectileProperties, now it will also set layer.

---
+++ Assets/Scripts/Enemies/Ships/EnemyShooter.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyShooter : EnemyBase
+{
+    [Header("Configuration")]
+    [Tooltip("Constant speed of movement.")]
+    public Vector2 cruiserSpeed;
+    [Tooltip("Thruster acceleration speed.")]
+    public float accelerationSpeed;
+    [Tooltip("Rotation speed in degrees per second.")]
+    public float rotationSpeed;
+    [Tooltip("Weapon configuration.")]
+    public Weapon weapon;
+    
+    private Rigidbody2D thisRigidbody2D;
+
+    protected override void Start()
+    {
+        thisRigidbody2D = rigidbodyHelper.GetRigidbody2D();
+        base.Start();
+    }
+
+    private void Update()
+    {
+        // Slowly accelerate to crusierSpeed
+        Vector2 speedToReachCrusier = cruiserSpeed - thisRigidbody2D.velocity;
+        Vector2 forceToReachCrusier = speedToReachCrusier * thisRigidbody2D.mass;
+        Vector2 forceToApply = Vector2.ClampMagnitude(forceToReachCrusier * Time.deltaTime, accelerationSpeed);
+        thisRigidbody2D.AddRelativeForce(forceToApply);
+
+        // Shoot
+        if (weapon.Recharge(Time.deltaTime))
+            weapon.Shoot(rigidbodyHelper, Instantiate);
+    }
+}
