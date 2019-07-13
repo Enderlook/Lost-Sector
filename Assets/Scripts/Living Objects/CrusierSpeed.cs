753971af0fbbf9de908515361ca094b2358a2d57
Ender Look <enderlook@gmail.com>
2019-07-12 23:39:33 -0300

    Make Cruise Speed component for enemies and add weapons to enemies
    Remake, move and rename Assets/Scripts/Enemies/Asteroids/EnemyShooter.cs into Assets/Scripts/Living Object/CrusierSpeed.cs.
    Removed Assets/Scripts/Enemies/Ships/.
    Add Enemy.weapons to store weapons.

---
+++ Assets/Scripts/Living Objects/CrusierSpeed.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+namespace LivingObjectAddons
+{
+    public class CrusierSpeed : Movement
+    {
+        [Header("Configuration")]
+        [Tooltip("Constant speed of movement.")]
+        public Vector2 cruiserSpeed;
+        [Tooltip("Thruster acceleration speed.")]
+        public float accelerationSpeed;
+
+        private Rigidbody2D thisRigidbody;
+        public override void OnBuild(LivingObject livingObject) => thisRigidbody = livingObject.rigidbodyHelper.GetRigidbody2D();
+
+        public override void Move()
+        {
+            // Slowly accelerate to crusierSpeed
+            Vector2 speedToReachCrusier = cruiserSpeed - thisRigidbody.velocity;
+            Vector2 forceToReachCrusier = speedToReachCrusier * thisRigidbody.mass;
+            Vector2 forceToApply = Vector2.ClampMagnitude(forceToReachCrusier * Time.deltaTime, accelerationSpeed);
+            thisRigidbody.AddRelativeForce(forceToApply);
+        }
+    }
+}
