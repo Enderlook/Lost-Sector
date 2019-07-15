5b929348118da5fb3c8931d24a2854813d40c984
Ender Look <enderlook@gmail.com>
2019-07-15 00:11:58 -0300

    Moved HealthPack.cs and Coin.cs from Assets/Scripts/Others to the new directory Assets/Scripts/Pickups/
    Make Pickupable abstract class. Now Coin and HealthPack inherits from it.

---
+++ Assets/Scripts/Pickups/HealthPack.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthPack : Pickupable
+{
+    [Header("Configuration")]
+    [Tooltip("Health restored on pick up.")]
+    public int healthRestored;
+    public override void Pickup(LivingObject livingObject) => livingObject.TakeHealing(healthRestored);
+}
---
+++ Assets/Scripts/Pickups/Pickupable.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Pickupable : MonoBehaviour, ICanBePickedUp
+{
+    [Header("Configuration")]
+    [Tooltip("Initial impulse.")]
+    public Vector2RangeTwo impulse;
+
+    private Rigidbody2D thisRigidbody2D;
+
+    Rigidbody2D ICanBePickedUp.Rigidbody2D => thisRigidbody2D;
+
+    private void Start()
+    {
+        thisRigidbody2D = gameObject.GetComponent<Rigidbody2D>();
+        thisRigidbody2D.AddRelativeForce((Vector2)impulse * thisRigidbody2D.mass);
+    }
+    protected virtual void OnValidate()
+    {
+        Rigidbody2D rigidbody2D = GetComponent<Rigidbody2D>();
+        if (rigidbody2D == null)
+            Debug.LogWarning($"Game object {gameObject.name} lacks of an Rigidbody2D Component.");
+    }
+
+    public abstract void Pickup(LivingObject livingObject);
+}
