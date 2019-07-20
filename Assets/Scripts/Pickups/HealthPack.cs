7de2cb9d47ed88a0e5f711334305fa49975e653c
Ender Look <enderlook@gmail.com>
2019-07-20 20:28:42 -0300

    Replaced LivingObject to Player in Pickupable to narrow usage

--- Assets/Scripts/Pickups/HealthPack.cs
+++ Assets/Scripts/Pickups/HealthPack.cs
@@ -7,5 +7,5 @@ public class HealthPack : Pickupable
     [Header("Configuration")]
     [Tooltip("Health restored on pick up.")]
     public int healthRestored;
-    public override void Pickup(LivingObject livingObject) => livingObject.TakeHealing(healthRestored);
+    public override void Pickup(Player player) => player.TakeHealing(healthRestored);
 }
