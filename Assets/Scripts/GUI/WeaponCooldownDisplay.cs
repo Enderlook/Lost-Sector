c1e27138a75f28a7909b78332b6bbec554c6d6fc
Enderlook <enderlook@gmail.com>
2019-07-23 13:52:59 -0300

    Add ExplosiveBurstWeapon cooldown display to Player in Level_1 Scene

---
+++ Assets/Scripts/GUI/WeaponCooldownDisplay.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class WeaponCooldownDisplay : MonoBehaviour
+{
+    [Header("Configuration")]
+    [Tooltip("Background sprite of weapon.")]
+    public Image image;
+
+    [Header("Setup")]
+    [Tooltip("Weapon to track cooldown")]
+    public LivingObjectAddons.Weapon weapon;
+
+    private void Update()
+    {
+        image.fillAmount = weapon.CooldownPercent;
+    }
+}
