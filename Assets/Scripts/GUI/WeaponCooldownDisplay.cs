f05572f6f950c83d8fdec05b0e9f9074910d4669
Enderlook <enderlook@gmail.com>
2019-07-23 15:26:41 -0300

    ExplosiveBurstWeapon can be shooted at half charge
    In addition. Weapon.CooldownPercent was clamped from 0 to 1 and Weapon.CanShoot was changed to virtual.
    In ExplosiveBurstWeapon renamed CalculatePower to CalculateDistanceFactor. Implement half charge shooting and CalculateChargeFactor method.
    Add in WeaponCooldownDisplay reverseFill and colors fields, and ColorPercent class. Color can be change according to cooldown percent.

--- Assets/Scripts/GUI/WeaponCooldownDisplay.cs
+++ Assets/Scripts/GUI/WeaponCooldownDisplay.cs
@@ -1,11 +1,16 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
+using System.Linq;
 
 public class WeaponCooldownDisplay : MonoBehaviour
 {
     [Header("Configuration")]
     [Tooltip("Background sprite of weapon.")]
     public Image image;
+    [Tooltip("Reverse fill mode.")]
+    public bool reverseFill;
+    [Tooltip("Colors to use.")]
+    public ColorPercent[] colors;
 
     [Header("Setup")]
     [Tooltip("Weapon to track cooldown")]
@@ -13,6 +18,51 @@ public class WeaponCooldownDisplay : MonoBehaviour
 
     private void Update()
     {
-        image.fillAmount = weapon.CooldownPercent;
+        image.fillAmount = reverseFill ? 1 - weapon.CooldownPercent : weapon.CooldownPercent;
+        if (colors.Length > 0)
+            image.color = GetColor(1 - weapon.CooldownPercent);
+    }
+
+    private Color GetColor(float percent)
+    {
+        for (int i = colors.Length - 1; i >= 0; i--)
+        {
+            if (colors[i].percent <= percent)
+                return colors[i].color;
+        }
+        throw new System.Exception("No matching color found. This shouldn't be happening.");
+    }
+
+#if UNITY_EDITOR
+    private void OnValidate()
+    {
+        if (colors.Length > 0)
+        {
+            if (colors[0].percent != 0)
+            {
+                Debug.LogWarning($"Gameobject {gameObject.name} has a the element on index 1 from {nameof(colors)} with a {nameof(ColorPercent.percent)} non equal to 0 ({colors[0].percent}). It was fixed.");
+                colors[0].percent = 0;
+            }
+            for (int i = 0; i < colors.Length - 1; i++)
+            {
+                int next = i + 1;
+                if (colors[i].percent >= colors[next].percent)
+                {
+                    Debug.LogWarning($"Gameobject {gameObject.name} has a the element on index {next} from {nameof(colors)} array with a {nameof(ColorPercent.percent)} ({colors[next].percent}) lower or equal than its precessor ({colors[i].percent}).\nIt should be greater.");
+                    colors[next].percent = colors[i].percent;
+                }
+            }
+        }
+    }
+#endif
+
+    [System.Serializable]
+    public class ColorPercent
+    {
+        [Tooltip("Color to show.")]
+        public Color color;
+        [Range(0, 1)]
+        [Tooltip("Minimal fill amount to show it")]
+        public float percent;
     }
 }
