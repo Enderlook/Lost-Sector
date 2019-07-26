a475630ac662b766a4744dc7809aaa8873ad8235
Ender Look <enderlook@gmail.com>
2019-07-26 20:51:53 -0300

    Add WeaponCooldownDisplay sound signal

--- Assets/Scripts/GUI/WeaponCooldownDisplay.cs
+++ Assets/Scripts/GUI/WeaponCooldownDisplay.cs
@@ -8,47 +8,59 @@ public class WeaponCooldownDisplay : MonoBehaviour
     public Image image;
     [Tooltip("Reverse fill mode.")]
     public bool reverseFill;
-    [Tooltip("Colors to use.")]
-    public ColorPercent[] colors;
+    [Tooltip("Configuration by percent.")]
+    public ModePercent[] modes;
 
     [Header("Setup")]
     [Tooltip("Weapon to track cooldown")]
     public LivingObjectAddons.Weapon weapon;
+    [Tooltip("Audio Source to play sounds.")]
+    public AudioSource audioSource;
 
+    private Sound lastSound;
+    
     private void Update()
     {
-        image.fillAmount = reverseFill ? 1 - weapon.CooldownPercent : weapon.CooldownPercent;
-        if (colors.Length > 0)
-            image.color = GetColor(1 - weapon.CooldownPercent);
-    }
-
-    private Color GetColor(float percent)
-    {
-        for (int i = colors.Length - 1; i >= 0; i--)
+        float charge = reverseFill ? 1 - weapon.CooldownPercent : weapon.CooldownPercent;
+        image.fillAmount = charge;
+        if (modes.Length > 0)
         {
-            if (colors[i].percent <= percent)
-                return colors[i].color;
+            ModePercent mode = GetMode(1 - weapon.CooldownPercent);
+            image.color = mode.color;
+            if (audioSource != null && mode.sound != lastSound && lastSound != null && mode.sound != null)
+                mode.sound.Play(audioSource, Settings.IsSoundActive);
+            lastSound = mode.sound;
         }
-        throw new System.Exception("No matching color found. This shouldn't be happening.");
+    }
+    private float GetCharge() => reverseFill ? 1 - weapon.CooldownPercent : weapon.CooldownPercent;
+
+    private ModePercent GetMode(float percent)
+    {
+        for (int i = modes.Length - 1; i >= 0; i--)
+        {
+            if (modes[i].percent <= percent)
+                return modes[i];
+        }
+        throw new System.Exception("No matching mode found. This shouldn't be happening.");
     }
 
 #if UNITY_EDITOR
     private void OnValidate()
     {
-        if (colors.Length > 0)
+        if (modes.Length > 0)
         {
-            if (colors[0].percent != 0)
+            if (modes[0].percent != 0)
             {
-                Debug.LogWarning($"Gameobject {gameObject.name} has a the element on index 1 from {nameof(colors)} with a {nameof(ColorPercent.percent)} non equal to 0 ({colors[0].percent}). It was fixed.");
-                colors[0].percent = 0;
+                Debug.LogWarning($"Gameobject {gameObject.name} has a the element on index 1 from {nameof(modes)} with a {nameof(ModePercent.percent)} non equal to 0 ({modes[0].percent}). It was fixed.");
+                modes[0].percent = 0;
             }
-            for (int i = 0; i < colors.Length - 1; i++)
+            for (int i = 0; i < modes.Length - 1; i++)
             {
                 int next = i + 1;
-                if (colors[i].percent >= colors[next].percent)
+                if (modes[i].percent >= modes[next].percent)
                 {
-                    Debug.LogWarning($"Gameobject {gameObject.name} has a the element on index {next} from {nameof(colors)} array with a {nameof(ColorPercent.percent)} ({colors[next].percent}) lower or equal than its precessor ({colors[i].percent}).\nIt should be greater.");
-                    colors[next].percent = colors[i].percent;
+                    Debug.LogWarning($"Gameobject {gameObject.name} has a the element on index {next} from {nameof(modes)} array with a {nameof(ModePercent.percent)} ({modes[next].percent}) lower or equal than its predecessor ({modes[i].percent}).\nIt should be greater.");
+                    modes[next].percent = modes[i].percent;
                 }
             }
         }
@@ -56,10 +68,12 @@ public class WeaponCooldownDisplay : MonoBehaviour
 #endif
 
     [System.Serializable]
-    public class ColorPercent
+    public class ModePercent
     {
         [Tooltip("Color to show.")]
         public Color color;
+        [Tooltip("Sound to play")]
+        public Sound sound;
         [Range(0, 1)]
         [Tooltip("Minimal fill amount to show it")]
         public float percent;
